86bb679f9d8a9c3e1134d272345a6b27efe79509
zamatolosnmtolo <zamatolosntmtolo@gmail.com>
2024-04-24 01:50:23 -0700

    Update Firebase

--- src/App.js
+++ src/App.js
@@ -8,7 +8,16 @@ import firebase from 'firebase/compat/app';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/auth'
 
-firebase.initializeApp({})
+firebase.initializeApp({  apiKey: "AIzaSyDnB3pL_OA7sCsLK3nJJVmTtELhPZhOEi8",
+authDomain: "talkchat-4ecb5.firebaseapp.com",
+projectId: "talkchat-4ecb5",
+storageBucket: "talkchat-4ecb5.appspot.com",
+messagingSenderId: "246912064543",
+appId: "1:246912064543:web:1929997bf1d6a723fb892a",
+measurementId: "G-1W07JSELSC"})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 function App() {
   return (
