7d9cd4d8a7eed8ebdd6ab5a41bb3f4301f6fafaf
zamatolosnmtolo <zamatolosntmtolo@gmail.com>
2024-04-24 01:31:13 -0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+import firebase from 'firebase/compat/app'; 
+     import 'firebase/compat/firestore';
+     import 'firebase/compat/auth';         
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/auth'
+
+firebase.initializeApp({})
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
