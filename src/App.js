ea5da065e7a0fadab8b8f5ce32b01db103b6020d
zamatolosnmtolo <zamatolosntmtolo@gmail.com>
2024-04-24 02:11:41 -0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React, { useRef, useState} from 'react';
 import './App.css';
 
-import firebase from 'firebase/compat/app'; 
-     import 'firebase/compat/firestore';
-     import 'firebase/compat/auth'; 
-     import 'firebase/analytics';        
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+import 'firebase/compat/analytics';
+     
 
 import { useAuthState } from 'react-firebase-hooks/auth';
-import { useCollectionData } from 'react-firebase-hooks/auth'
+import { useCollectionData } from 'react-firebase-hooks/firestore';
 
-firebase.initializeApp({  apiKey: "AIzaSyDnB3pL_OA7sCsLK3nJJVmTtELhPZhOEi8",
-authDomain: "talkchat-4ecb5.firebaseapp.com",
-projectId: "talkchat-4ecb5",
-storageBucket: "talkchat-4ecb5.appspot.com",
-messagingSenderId: "246912064543",
-appId: "1:246912064543:web:1929997bf1d6a723fb892a",
-measurementId: "G-1W07JSELSC"})
+firebase.initializeApp({
+  apiKey: "AIzaSyDnB3pL_OA7sCsLK3nJJVmTtELhPZhOEi8",
+  authDomain: "talkchat-4ecb5.firebaseapp.com",
+  projectId: "talkchat-4ecb5",
+  storageBucket: "talkchat-4ecb5.appspot.com",
+  messagingSenderId: "246912064543",
+  appId: "1:246912064543:web:1929997bf1d6a723fb892a",
+  measurementId: "G-1W07JSELSC"
+});
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
